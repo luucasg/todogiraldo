6fe89b52f5c74a12f7a450b14cf1f7f96177b402
Lucas Giraldo <lucasgcorrales@gmail.com>
2021-08-24 20:25:51 -0500

    Add entity

---
+++ todogiraldo.Common/Responses/Response.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace todogiraldo.Common.Responses
+{
+    public class Response
+    {
+        public bool IsSucces { get; set; }
+        public string Message { get; set; }
+        public object Result { get; set; }
+    }
+}
---
+++ todogiraldo.Functions/Entities/TodoEntity.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace todogiraldo.Functions.Entities
+{
+    public class TodoEntity : TableEntity
+    {
+        public DateTime CreatedTime { get; set; }
+        public string TaskDescription { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
