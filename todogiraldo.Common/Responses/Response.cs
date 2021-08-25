beff3e18fe8da0043ce380fea876f6ec1f6d7f9b
Lucas Giraldo <lucasgcorrales@gmail.com>
2021-08-24 21:58:30 -0500

    Update Todo

--- todogiraldo.Common/Responses/Response.cs
+++ todogiraldo.Common/Responses/Response.cs
@@ -6,7 +6,7 @@ namespace todogiraldo.Common.Responses
 {
     public class Response
     {
-        public bool IsSucces { get; set; }
+        public bool IsSuccess { get; set; }
         public string Message { get; set; }
         public object Result { get; set; }
     }
