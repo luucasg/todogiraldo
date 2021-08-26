20364a2a15bc8ea69e7e1d93656280d0f13cfbc1
Lucas Giraldo <lucasgcorrales@gmail.com>
2021-08-25 21:01:44 -0500

    Pruebas unitarias y fin del código

---
+++ todogiraldo.Test/Test/ScheduledFuctionTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using todogiraldo.Functions.Functions;
+using todogiraldo.Test.Helpers;
+using Xunit;
+
+namespace todogiraldo.Test.Test
+{
+    public class ScheduledFuctionTest
+    {
+
+        [Fact]
+        public void ScheduledFuction_Should_Log_Message()
+        {
+            // Arrange
+            MockCloudTableTodos mockTodos = new MockCloudTableTodos(new Uri("http://127.0.0.1:10002/devstoreaccount1/reports"));
+            ListLogger logger = (ListLogger)TestFactory.CreateLogger(LoggerTypes.List);
+
+            // Add
+            ScheduleFunction.Run(null, mockTodos, logger);
+            string message = logger.Logs[0];
+
+            // Assert
+            Assert.Contains("Deleting completed", message);
+        }
+    }
+}
